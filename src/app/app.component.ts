cfee78b36f83c06df18046b422a8f9da299ed2d8
Matthew <jmatthewpareja@gmail.com>
2025-02-19 00:21:41 +0100

    Ce fini

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,39 @@
 import { Component } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: false,
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  isPerfilMenuOpen = false;
+  isDarkMode = false;
   title = 'digimonAPI';
+  logged = false;
+
+  constructor(private router: Router) {
+    this.logged = localStorage.getItem('logged') === 'true'; //Comprueba si esta logeado
+    if (this.logged) {
+      this.isDarkMode = localStorage.getItem('darkMode') === 'enabled'; //Comprueba el tema
+    }
+  }
+
+  menuPerfil(){
+    this.isPerfilMenuOpen = !this.isPerfilMenuOpen;
+  }
+  cerrarSesion(){
+    this.logged = false;
+    localStorage.setItem('logged','false');
+    this.isDarkMode = false;
+    localStorage.setItem('darkMode', this.isDarkMode ? 'enabled' : 'disabled');
+    location.reload();
+    localStorage.setItem('favs', "");
+  }
+  changeTheme(){
+    this.isDarkMode = !this.isDarkMode;
+    localStorage.setItem('darkMode', this.isDarkMode ? 'enabled' : 'disabled');
+    location.reload();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,43 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {RouterModule, Routes} from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { BuscadorComponent } from './buscador/buscador.component';
+import { InfoComponent } from './info/info.component';
+import { DigimonListComponent } from './buscador/digimon-list/digimon-list.component';
+import { FavoritosComponent } from './buscador/favoritos/favoritos.component';
+import {FormsModule} from '@angular/forms';
+import { DigimonDetallesComponent } from './buscador/digimon-detalles/digimon-detalles.component';
+import {LoginService} from './login.service';
+import {UsersService} from './users.service';
+
+const appRoutes: Routes = [
+  {path: '', component: InicioComponent},
+  {path: 'buscador', component: BuscadorComponent},
+  {path: 'info',component: InfoComponent},
+  {path: 'digimonlist', component: DigimonListComponent},
+  {path: 'favoritos', component: FavoritosComponent},
+  {path: 'detalles/:name', component: DigimonDetallesComponent},
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    BuscadorComponent,
+    InfoComponent,
+    DigimonListComponent,
+    FavoritosComponent,
+    DigimonDetallesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes),
+    FormsModule
   ],
-  providers: [],
+  providers: [LoginService, UsersService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
